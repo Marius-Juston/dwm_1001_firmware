120e195718cf314c56d507ddf6434490bf408e21
Marius Juston <Marius.juston@hotmail.fr>
2025-04-18 05:45:02 -0500

    updated code

--- examples/dwm-twr-ranges/dwm-simple.c
+++ examples/dwm-twr-ranges/dwm-simple.c
@@ -35,12 +35,22 @@ void on_dwm_evt(dwm_evt_t *p_evt)
 {
 	int len;
 	int i;
+        int n;
 
 	switch (p_evt->header.id) {
 	/* New location data */
 	case DWM_EVT_LOC_READY:
-		for (i = 0; i < p_evt->loc.anchors.dist.cnt; ++i) {
+          n = p_evt->loc.anchors.dist.cnt; 
+
+          printf("CNT,%u", n);
+
+		for (i = 0; i < n; ++i) {
+                       if(i > 0)
+                       printf(",");
+
 			printf("%04X,", (unsigned int)(p_evt->loc.anchors.dist.addr[i] & 0xffff));
+                      
+
 			if (i < p_evt->loc.anchors.an_pos.cnt) {
 				printf("%ld,%ld,%ld,",
 						p_evt->loc.anchors.an_pos.pos[i].x,
@@ -57,6 +67,54 @@ void on_dwm_evt(dwm_evt_t *p_evt)
 	}
 }
 
+
+void config_tag(void){
+	int rv;
+	dwm_cfg_t cfg;
+	dwm_cfg_tag_t  cfg_tag;
+
+	/* Get node configuration */
+	APP_ERR_CHECK(dwm_cfg_get(&cfg));
+
+	/* Configure device as TAG */
+	cfg_tag.stnry_en = false;
+	cfg_tag.loc_engine_en = false;
+	cfg_tag.low_power_en = false;
+	cfg_tag.meas_mode = DWM_MEAS_MODE_TWR;
+	cfg_tag.common.fw_update_en = false;
+	cfg_tag.common.uwb_mode = DWM_UWB_MODE_ACTIVE;
+	cfg_tag.common.ble_en = false;
+	cfg_tag.common.led_en = true;
+	cfg_tag.common.enc_en = false;
+
+	if ((cfg.mode != DWM_MODE_TAG) ||
+	(cfg.stnry_en != cfg_tag.stnry_en) ||
+	(cfg.loc_engine_en != cfg_tag.loc_engine_en) ||
+	(cfg.low_power_en != cfg_tag.low_power_en) ||
+	(cfg.meas_mode != cfg_tag.meas_mode) ||
+	(cfg.common.fw_update_en != cfg_tag.common.fw_update_en) ||
+	(cfg.common.uwb_mode != cfg_tag.common.uwb_mode) ||
+	(cfg.common.ble_en != cfg_tag.common.ble_en) ||
+	(cfg.common.led_en != cfg_tag.common.led_en)) {
+
+		if(cfg.mode 			!= DWM_MODE_TAG) 		printf("mode: get = %d, set = %d\n", cfg.mode, 		DWM_MODE_ANCHOR);
+		if(cfg.stnry_en     		!= cfg_tag.stnry_en)  		printf("acce: get = %d, set = %d\n", cfg.stnry_en, 	cfg_tag.stnry_en);
+		if(cfg.loc_engine_en 		!= cfg_tag.loc_engine_en) 	printf("le  : get = %d, set = %d\n", cfg.loc_engine_en, cfg_tag.loc_engine_en);
+		if(cfg.low_power_en		!= cfg_tag.low_power_en)	printf("lp  : get = %d, set = %d\n", cfg.low_power_en, 	cfg_tag.low_power_en);
+		if(cfg.meas_mode 		!= cfg_tag.meas_mode) 		printf("meas: get = %d, set = %d\n", cfg.meas_mode, 	cfg_tag.meas_mode);
+		if(cfg.common.fw_update_en 	!= cfg_tag.common.fw_update_en)	
+										printf("fwup: get = %d, set = %d\n", cfg.common.fw_update_en, cfg_tag.common.fw_update_en);
+		if(cfg.common.uwb_mode		!= cfg_tag.common.uwb_mode)	printf("uwb : get = %d, set = %d\n", cfg.common.uwb_mode, cfg_tag.common.uwb_mode);
+		if(cfg.common.ble_en 		!= cfg_tag.common.ble_en)	printf("ble : get = %d, set = %d\n", cfg.common.ble_en, cfg_tag.common.ble_en);
+		if(cfg.common.enc_en 		!= cfg_tag.common.enc_en)	printf("enc : get = %d, set = %d\n", cfg.common.enc_en, cfg_tag.common.enc_en);
+		if(cfg.common.led_en 		!= cfg_tag.common.led_en)	printf("led : get = %d, set = %d\n", cfg.common.led_en, cfg_tag.common.led_en);
+
+		APP_ERR_CHECK(rv = dwm_cfg_tag_set(&cfg_tag));
+
+		dwm_reset();
+	}  
+}
+
 /**
  * Application thread
  *
@@ -64,18 +122,13 @@ void on_dwm_evt(dwm_evt_t *p_evt)
  */
 void app_thread_entry(uint32_t data)
 {
-	dwm_cfg_t cfg;
 	dwm_evt_t evt;
 	int rv;
+        config_tag();
 
-	/* Get node configuration */
-	APP_ERR_CHECK(dwm_cfg_get(&cfg));
 
 	/* Update rate set to 1 second, stationary update rate set to 5 seconds */
 	APP_ERR_CHECK(dwm_upd_rate_set(1, 1));
-
-	/* Sensitivity for switching between stationary and normal update rate */
-	APP_ERR_CHECK(dwm_stnry_cfg_set(DWM_STNRY_SENSITIVITY_NORMAL));
 
 	/* Register event callback */
 	dwm_evt_listener_register(
